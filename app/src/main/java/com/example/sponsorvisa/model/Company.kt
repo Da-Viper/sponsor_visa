6cf45713dc494420e1c7e6301059bbe479543117
Ezike Ebuka <yerimyah1@gmail.com>
2022-09-14 22:20:23 +0100

    add: data class for company

---
+++ app/src/main/java/com/example/sponsorvisa/model/Company.kt
@@ -0,0 +1,9 @@
+package com.example.sponsorvisa.model
+
+data class Company(
+    val name: String,
+    val city: String?,
+    val rating: String?,
+    val route: String,
+)
+//city, county,rating, route
