41f3df48b1c3093d253898460c57a8fd2c0b881f
Ezike Ebuka <yerimyah1@gmail.com>
2022-09-18 03:06:46 +0100

    add (Main Activity): Remove Header Bar

--- app/src/main/java/com/example/sponsorvisa/MainActivity.kt
+++ app/src/main/java/com/example/sponsorvisa/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.sponsorvisa
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
     }
 }
